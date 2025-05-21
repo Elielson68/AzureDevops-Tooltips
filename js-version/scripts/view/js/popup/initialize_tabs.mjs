94282cd7e39aa63717d59f5d57e1abd60a636df5
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-05-21 09:01:01 -0300

    feat: add scripts de cada tela ao abrir ela

--- js-version/scripts/view/js/popup/initialize_tabs.mjs
+++ js-version/scripts/view/js/popup/initialize_tabs.mjs
@@ -1,4 +1,5 @@
 let lastTab = null;
+let lastScript = null;
 
 export async function StartModule() {
     async function loadTabContent(navButton, tabName) {
@@ -6,13 +7,20 @@ export async function StartModule() {
         lastTab = navButton;
         navButton.classList.add('active');
 
+        if (lastScript) {
+            lastScript.remove();
+            lastScript = null;
+        }
+
         const response = await fetch(`/scripts/view/html/popup/${tabName}.html`);
         const html = await response.text();
         document.getElementById('tab-content').innerHTML = html;
 
-        // const script = document.createElement('script');
-        // script.src = `./popup/${tabName}.mjs`;
-        // document.body.appendChild(script);
+
+        const script = document.createElement('script');
+        script.src = `/scripts/view/js/popup/${tabName}.mjs`;
+        document.body.appendChild(script);
+        lastScript = script;
     }
 
     loadTabContent(document.querySelectorAll('.nav-tab')[0], 'main');
