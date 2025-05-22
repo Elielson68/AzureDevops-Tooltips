1684eab24f868e5ec4976f17fcc1b3eb8bdfb04e
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-05-22 07:47:29 -0300

    feat: modulariza e isola responsabilidade do script main da view

--- js-version/scripts/view/js/popup/main.mjs
+++ js-version/scripts/view/js/popup/main.mjs
@@ -1,16 +1,13 @@
-console.log("Carregou main.mjs");
-chrome.storage.local.get(['azureConfig'], (result) => {
-    if (result.azureConfig) {
-        token.value = result.azureConfig.token;
-    }
-});
+const tokenInput = document.getElementById('token');
 
+export function setInputTokenValue(token) {
+    tokenInput.value = token;
+}
 
-document.getElementById("saveToken").addEventListener('click', async () => {
-    const tokenInput = document.getElementById('token');
-    const token = (tokenInput && tokenInput.value);
+export function getInputTokenValue() {
+    return tokenInput.value;
+}
 
-    chrome.storage.local.set({ azureConfig: { token } }, () => {
-        alert('Token salvo com sucesso!');
-    });
-});
+export function setEventListenerToSaveTokenButton(event) {
+    document.getElementById("saveToken").addEventListener('click', event);
+}
