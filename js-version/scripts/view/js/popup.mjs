8f050fe97ffba696b1c7f9ed1756c9562dbb7bde
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-05-22 07:47:46 -0300

    refact: altera forma de importar os dados

--- js-version/scripts/view/js/popup.mjs
+++ js-version/scripts/view/js/popup.mjs
@@ -1,13 +1,3 @@
-const initializeModulePath = "/scripts/view/js/popup/initialize_tabs.mjs";
+import { StartModule } from "./popup/initialize_tabs.mjs";
 
-async function initializeModules(modulePath) {
-    try {
-        const module = await import(chrome.runtime.getURL(modulePath));
-        await module.StartModule();
-    } catch (error) {
-        console.error(`Erro ao carregar o módulo ${modulePath}:`, error);
-    }
-}
-
-initializeModules(initializeModulePath);
-
+StartModule();
