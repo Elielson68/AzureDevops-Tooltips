8dd86399f7ff97e9ab8c0d8a810e3902fa6eb687
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-05-22 10:29:54 -0300

    feat: separando update_tasls no MVC

--- js-version/scripts/js/controller/update_tasks.mjs
+++ js-version/scripts/js/controller/update_tasks.mjs
@@ -1,6 +1,24 @@
-import { getAllTeamMembers } from './../model/api_azure.mjs';
+import { updateTasks } from "../model/update_tasks.mjs";
+import { addEventListenerToUpdateButton, getInputStoryIdValue, showLoading, showStatus } from "../view/update_tasks.mjs";
 
+async function startUpdateTasks() {
+    const storyId = getInputStoryIdValue();
+    if (!storyId) {
+        showStatus('Por favor, informe o ID da Story/Bug', 'error');
+        return;
+    }
 
-export async function StartModule() {
-    console.log(await getAllTeamMembers());
+    showLoading();
+    try {
+        updateTasks()
+        showStatus(`Sucesso! ${updatedCount} tasks atualizadas.`, 'success');
+    }
+    catch (error) {
+        showStatus(error.message, 'error');
+    }
+    finally {
+        hideLoading();
+    }
 }
+
+addEventListenerToUpdateButton(startUpdateTasks);
