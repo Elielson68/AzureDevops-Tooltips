c5457dfcebf38774fb92d9da13f1f892320fc89e
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-05-22 07:44:29 -0300

    chore: remove inicialização da API da Azure

--- js-version/scripts/module_manager.mjs
+++ js-version/scripts/module_manager.mjs
@@ -1,7 +1,5 @@
 import { StartModule as StartModal } from "./view/modal.mjs";
-import { StartModule as StartApiZure } from "./model/api_azure.mjs";
 
 export function StartModules() {
     StartModal();
-    StartApiZure();
 }
